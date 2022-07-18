24767b820c512a72b4233d1d24d4c610a171ace7
webdevprashant <prashantkumar69328@gmail.com>
2022-07-18 14:21:59 +0530

    18 july

---
+++ script.js
@@ -0,0 +1,6 @@
+function livehtmleditor()      {
+	var codewritearea = document.getElementById('codewrittendiv');
+	var idoftextarea  = document.getElementById('codearea');
+	
+	codewritearea.innerHTML = idoftextarea.value	
+}
